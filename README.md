28f5abbf556744c7bbeff5da72a60ccf8c58c084
Fleetwood <flyoungiv@gmail.com>
2023-07-25 10:28:27 -0500

    read from db and return pydantic model

--- README.md
+++ README.md
@@ -7,17 +7,16 @@ Link to frontend repo: https://github.com/flyoungiv/tomato
 - ~~FastAPI hello world~~
 - ~~generate sample data~~
 - ~~set up git repo https://github.com/flyoungiv/tomato~~
-- host BE with Deta https://fastapi.tiangolo.com/deployment/deta/
+- ~~host BE https://fastapi.tiangolo.com/deployment/deta/~~
 - ~~fix CORS issues for local~~
-- host a db
-- inject data to db
-- read data from db
+- ~~host a db~~
+- ~~inject data to db~~
+- ~~read data from db~~
 - pagination
 - search
 - filter
 - sort
 - graph 1: correlation between population and number of medals
-- likelihood of reappearance if you win a medal
 - ~~create google cloud db~~
 - normalize db table so each contestant has its own uuid
 - introduce an ORM (sqlAlchemy?)
@@ -36,8 +35,10 @@ https://www.worlddata.info/countrycodes.php
 
 ## Notes & Thoughts
 =======
-i could have saved time by only taking matches on ISO and NOC country codes but i wanted to find a solution because real life data is messy
-i don't have homebrew. i don't have anything. what a time to format my computer
-had to find a lookup table for country codes to convert from olympics to iso
-did not get to mobile
-would be better to have done data transformation within endpoint instead of in a separate script
+- i could have saved time by only taking matches on ISO and NOC country codes but i wanted to find a solution because real life data is messy
+- i don't have homebrew. i don't have anything. what a time to format my computer
+- had to find a lookup table for country codes to convert from olympics to iso
+- did not get to mobile
+- would be better to have done data transformation within endpoint instead of in a separate script
+- did not get to auth
+- likelihood of reappearance if you win a medal
--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from pydantic import BaseModel, PositiveInt
 
 from fastapi.middleware.cors import CORSMiddleware
 
-# import psycopg2
+import psycopg2
+
+DB_HOST = "34.121.201.240"
+DB_USER = "postgres"
+DB_PASSWORD = ";u/)Ahzy)\"ZE8Vc&"
 
 app = FastAPI()
 
-#CORS
+# CORS
 origins = [
     "http://localhost",
     "http://localhost:3000",
@@ -26,63 +30,72 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
 
 class OlympicCountry(BaseModel):
     country: str
     country_code: str
     medals: PositiveInt
 
+
 @app.get("/")
 def read_root():
     return {"Congratulations": "You found the backend"}
 
+
 @app.get("/countries")
 def read_item() -> list[OlympicCountry]:
-    # conn = psycopg2.connect(database = "postgres", user = "postgres", password = ";u/)Ahzy)\"ZE8Vc&", host = "34.121.201.240", port = "5432")
-    # cur = conn.cursor()
+    conn = psycopg2.connect(database="postgres", user=DB_USER,
+                            password=DB_PASSWORD, host=DB_HOST, port="5432")
+    cur = conn.cursor()
 
-    # cur.execute("""
-    # select
-    #   p.country,
-    #   cc.iso3 as country_code,
-    #   count(1) as medals
-    # from
-    #   contestants c
-    #   join country_codes cc on c.noc = cc.noc
-    #   join populations p on cc.iso3 = p.country_code
-    # where
-    #   c.year >= 1960
-    #   and c.year <= 2016
-    #   and c.medal is not null
-    # group by
-    #   p.country,
-    #   cc.iso3
-    # order by
-    #   country asc;
-    # """)
+    cur.execute("""
+      select
+        p.country,
+        cc.iso3 as country_code,
+        count(1) as medals
+      from
+        contestants c
+        join country_codes cc on c.noc = cc.noc
+        join populations p on cc.iso3 = p.country_code
+      where
+        c.year >= 1960
+        and c.year <= 2016
+        and c.medal is not null
+      group by
+        p.country,
+        cc.iso3
+      order by
+        country asc;
+    """)
 
-    # rows = cur.fetchall()
-    # conn.close()
-    country1 = {
-      "country": "Test1",
-      "country_code": "AAA",
-      "medals": 4
-    }
-    
-    country1 = OlympicCountry(**country1)
-    rows=[country1]
-    # return {"item_id": item_id, "q": q}
-    # olympic_countries = OlympicCountry(**rows)
-    return rows
+    rows = cur.fetchall()
+    conn.close()
 
-#use to get more in-depth detail on country
+    countries = []
+    for row in rows:
+        country = {
+            "country": row[0],
+            "country_code": row[1],
+            "medals": row[2]
+        }
+
+        country = OlympicCountry(**country)
+        countries.append(country)
+
+    return countries
+
+# use to get more in-depth detail on country
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=os.environ['PORT'], log_level="info")
+    uvicorn.run("main:app", host='0.0.0.0',
+                port=os.environ['PORT'], log_level="info")

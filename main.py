a3e9d310a9e815e7f8bf46b734f672bf0c2c30c0
Fleetwood <flyoungiv@gmail.com>
2023-07-26 18:37:36 -0500

    local credentials for db

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import uvicorn
 import os
 from typing import Union
-
-from fastapi import FastAPI
 from pydantic import BaseModel, PositiveInt
 
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from google.cloud.sql.connector import Connector, IPTypes
 import pg8000
-
-import sqlalchemy
-
 import sqlalchemy
 from sqlalchemy import text
-
-# import psycopg2
 
 DB_HOST = "34.121.201.240"
 DB_USER = "postgres"
@@ -101,7 +95,7 @@ def get_olympic_country_detail(country_code: int):
     return {"item_name": item.name, "country_code": country_code}
 
 @app.get("/sqlalchemy")
-def read_root():
+def connect_with_sql_alchemy() -> list[OlympicCountry]:
     import os
 
     def connect_with_connector() -> sqlalchemy.engine.base.Engine:
@@ -148,12 +142,47 @@ def read_root():
         return pool
 
     engine = connect_with_connector()
+    
+    rows = []
 
     with engine.connect() as conn:
-        result = conn.execute(text("select 'hello world'"))
-        print(result.all())
+        result = conn.execute(text("""
+            select
+                p.country,
+                cc.iso3 as country_code,
+                count(1) as medals
+            from
+                contestants c
+                join country_codes cc on c.noc = cc.noc
+                join populations p on cc.iso3 = p.country_code
+            where
+                c.year >= 1960
+                and c.year <= 2016
+                and c.medal is not null
+            group by
+                p.country,
+                cc.iso3
+            order by
+                country asc;
+            """))
+        
+        rows = result.all()
+        print('rows')
+        print(rows)
 
-    return { "message": "done"}
+    countries = []
+
+    for row in rows:
+        country = {
+            "country": row[0],
+            "country_code": row[1],
+            "medals": row[2]
+        }
+
+        country = OlympicCountry(**country)
+        countries.append(country)
+
+    return countries
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0',
